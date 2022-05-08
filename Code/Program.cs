beed1e31e4250e02ffd6aac6c5ee3ecf73cb1535
Viola <viola-doc@yandex.ru>
2022-05-08 18:09:26 +0300

    Добавлен код

---
+++ Code/Program.cs
@@ -0,0 +1,27 @@
+﻿string [] array = new string [5] {"Звезда", "Дом","Кот","33","Небо"};
+
+string [] newArray = new string [array.Length];
+
+void Viborka( string[]array1, string[]massive)
+{
+    int count = 0;
+    for ( int i=0;i < array1.Length; i ++)
+    {
+        if (array1[i].Length <=3)
+        {
+           massive[count] = array1[i];
+           count ++;
+        }
+    }
+}
+
+void PrintArray (string[]array2)
+{
+    for ( int i =0;i < array2.Length;i++)
+    {
+       Console.WriteLine ( $"{array2[i]}");
+       
+    }
+}
+Viborka(array,newArray);
+PrintArray(newArray);
